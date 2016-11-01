8d47fdbb757a513b46e8a76db38e2c3e28c93315
Joel Walker <joelwalker1995@gmail.com>
2016-11-01 04:09:37 +0000

    Source filtering is working now

--- server.R
+++ server.R
@@ -1,8 +1,23 @@
 library(shiny)
 
+arisa <- read.csv("./data/ARISA.CSV", header = TRUE)
+arisa$source <- substr(arisa$X, 1, 3)
+
 server <- function(input, output) {
   output$hist <- renderPlot({ 
     title <- "100 random normal values"
-    hist(rnorm(input$num), main = title) 
+    hist(rnorm(100), main = title)
+  })
+
+  output$arisa_plot <- renderPlot({
+    filtered <- arisa[arisa$source %in% c(if(input$tt1) "TT1" else NULL, if(input$tt2) "TT2" else NULL, if(input$tt3) "TT3" else NULL),]
+    filtered$X <- NULL
+    filtered$source <- NULL
+    
+    # Render a barplot
+    barplot(colSums(filtered),
+            main=input$region,
+            ylab="Number of Telephones",
+            xlab="Year")
   })
 }
--- ui.R
+++ ui.R
@@ -2,38 +2,58 @@ library(shiny)
 
 
 ui <- pageWithSidebar(
+  
+  # Application title
+  headerPanel("SidePanel"),
+  
+  # Sidebar with controls to select the variable to plot against mpg
+  # and to specify whether outliers should be included
+  sidebarPanel(
+  
+    selectInput("1", "1. Select",
+      choices=list(
+      'Colpodida'='Col','Cyrtophorida'='Cyr','Gymnostomatida'='Gym','Heterotrichida'='Het',
+      'Hymenostomatida'='Hym','Hypotrichida'='Hyp','Oligotrichida'='Oli','Peritrichida'='Per',
+      'Pleurostomatida'='Ple','Prostomatida'='Pro','Suctorida'='Suc'
+      )),
+      selectizeInput(
+      '2', '2. Multi-select', choices=list(
+      'Colpodida'='Col','Cyrtophorida'='Cyr','Gymnostomatida'='Gym','Heterotrichida'='Het',
+      'Hymenostomatida'='Hym','Hypotrichida'='Hyp','Oligotrichida'='Oli','Peritrichida'='Per',
+      'Pleurostomatida'='Ple','Prostomatida'='Pro','Suctorida'='Suc'
+      
+      #`New York` = 'NY', `New Jersey` = 'NJ',
+      #`California` = 'CA', `Washington` = 'WA'
+      ), multiple = TRUE
+    ),
+  
+    selectizeInput(
+      '3', '3. Site', choices=list(
+      'T1'='t1','T2'='t2','T3'='t3'
+      
+      #`New York` = 'NY', `New Jersey` = 'NJ',
+      #`California` = 'CA', `Washington` = 'WA'
+      ), multiple = TRUE),
+      sliderInput("integer", "Integer:",
+      min=0, max=1000, value=500
+    ),
+  
+    numericInput("num", "Number:",NULL),
+  
+    checkboxInput(inputId = "tt1",
+      label = strong("Show TT1"),
+      value = TRUE),
+      
+    checkboxInput(inputId = "tt2",
+      label = strong("Show TT2"),
+      value = TRUE),
+      
+    checkboxInput(inputId = "tt3",
+      label = strong("Show TT3"),
+      value = TRUE)
 
-# Application title
-headerPanel("SidePanel"),
-
-# Sidebar with controls to select the variable to plot against mpg
-# and to specify whether outliers should be included
-sidebarPanel(
-selectInput("1", "1. Select",
-choices=list(
-'Colpodida'='Col','Cyrtophorida'='Cyr','Gymnostomatida'='Gym','Heterotrichida'='Het',
-'Hymenostomatida'='Hym','Hypotrichida'='Hyp','Oligotrichida'='Oli','Peritrichida'='Per',
-'Pleurostomatida'='Ple','Prostomatida'='Pro','Suctorida'='Suc'
-)),
-selectizeInput(
-'2', '2. Multi-select', choices=list(
-'Colpodida'='Col','Cyrtophorida'='Cyr','Gymnostomatida'='Gym','Heterotrichida'='Het',
-'Hymenostomatida'='Hym','Hypotrichida'='Hyp','Oligotrichida'='Oli','Peritrichida'='Per',
-'Pleurostomatida'='Ple','Prostomatida'='Pro','Suctorida'='Suc'
-
-#`New York` = 'NY', `New Jersey` = 'NJ',
-#`California` = 'CA', `Washington` = 'WA'
-), multiple = TRUE),
-selectizeInput(
-'3', '3. Site', choices=list(
-'T1'='t1','T2'='t2','T3'='t3'
-
-#`New York` = 'NY', `New Jersey` = 'NJ',
-#`California` = 'CA', `Washington` = 'WA'
-), multiple = TRUE),
-sliderInput("integer", "Integer:",
-min=0, max=1000, value=500),
-numericInput("num", "Number:",NULL)
-),
-mainPanel()
+  ),
+  mainPanel(
+    plotOutput("arisa_plot")
+  )
 )
