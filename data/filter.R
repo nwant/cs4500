b905dc2f341ea3067ce62c8427247702513a5f5a
Nathan Want <nwant01@gmail.com>
2016-11-12 15:46:13 -0600

    changed interface for filter function to include parameter for blurring

--- data/filter.R
+++ data/filter.R
@@ -1,6 +1,15 @@
-filter <- function(df, species, sources, date.min, date.max) {
-  filtered <- df[colnmes(df) %in% species] 
-  filtered <- filtered[filtered$source %in% sources,]
+filter.all.data <- function(df, species, sources, date.min, date.max, blur) {
+  # filter by site location (e.g. T1, T2, and/or T3)
+  filtered <- df[df$source %in% sources,]
+  
+  # TODO: work with bluring
+  
+  # get appropriate data time slice
   filtered <- filtered[filtered$date >= date.min,]
   filtered <- filtered[filtered$date <= date.max,]
+  
+  # remove all columns from the dataframe except the columns for requested species
+  filtered <- filtered[colnames(filtered) %in% species] 
+ 
+  return(filtered)
 }
