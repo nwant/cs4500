2f569f5a1267997374f4d48fe18bebb99df6f110
Nathan Want <nwant01@gmail.com>
2016-11-12 15:04:59 -0600

    function added to get arisa and ciliates dataframes into one, merged by source

--- data/repo.R
+++ data/repo.R
@@ -1,8 +1,11 @@
+library("plyr")
+
 get.arisa <- function(config) {
     arisa <- read.csv(config[["arisa.fp"]], header = TRUE)
  
     # add column for classifying each testing site
-    arisa$source <- substr(arisa$X, 1, 3) 
+    arisa$source <- substr(arisa$X, 1, 3)
+    arisa$source <- gsub("TT", "T", arisa$source)
  
     # add column for each sample test date
     arisa$date <- as.Date(substr(arisa$X, 5, 100), "%m_%d_%y")
@@ -23,4 +26,24 @@ get.ciliates.1 <- function(config) {
     colnames(ciliates.1)[length(ciliates.1)] <- "TotalCellsPerLiter"
     
     return(ciliates.1)
+}
+
+get.all <- function(config) {
+  arisa <- get.arisa(config)
+  ciliates.1 <- get.ciliates.1(config)
+  
+  # stardardize each raw dataframe by removing unnecessary data columns and making 
+  ciliates.1 <- rename(ciliates.1, c("Date" = "date", "site" = "source"))
+  ciliates.1$TotalCellsPerLiter <- NULL
+  ciliates.1$month <- NULL
+  ciliates.1$year <- NULL
+  ciliates.1$X <- NULL
+  
+  ciliates.1$datesource <- paste(ciliates.1$date, ciliates.1$source, sep=" ")
+  arisa$datesource <- paste(arisa$date, arisa$source, sep=" ")
+  
+  # merge on date
+  merged.by.date <- merge(ciliates.1, arisa, by="datesource", all =T)
+  
+  return(merged.by.date)
 }
