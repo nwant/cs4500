7f293d4a6a299438e538f0371aacb82edd4fb80c
tjshbb <scott.t.3042@gmail.com>
2016-11-16 12:12:38 -0600

    Update filter.R

--- data/filter.R
+++ data/filter.R
@@ -1,17 +1,33 @@
+# This file filters dataframes by:
+#         1. test-site
+#         2. species classification
+#         3. min. and max date
+
+#Calling should look like: ???
+
+
+####[filter.all.data]##############################################################################
+# This function takes in (a dataframe, a test-ste, a species classification, minimum and maximum  #
+# for dates) as parameters.  Then it filters the dataframe by test-site.  Next that dataframe is  #
+# filtered by filtered by species classification.  After that, the dataframe is filtered by the   #
+# time-slice in-between the min. date and max. date.  Next that dataframe has all unnessecary     #
+# columns removed.  Finally that dateframe is returned.                                           #
+###################################################################################################
 filter.all.data <- function(df, sources, classes, date.min, date.max, blur=NULL, species="all") {
-  # filter by site location (e.g. T1, T2, and/or T3)
+  
+  # Filter by site location (e.g. T1, T2, and/or T3)
   filtered <- df[df$source %in% sources,]
    
-  # filter by species classification
+  # Filter by species classification
   filtered <- filtered[filtered$class %in% classes,]
   
   # TODO: work with bluring
   
-  # get appropriate data time slice
+  # Get appropriate data time slice
   filtered <- filtered[filtered$date >= date.min,]
   filtered <- filtered[filtered$date <= date.max,]
   
-  # remove all columns from the dataframe except the columns for requested species
+  # Remove all columns from the dataframe except the columns for requested species
   if (grepl(species, "all")) {
     filtered.date <- NULL
     filtered.source <- NULL
