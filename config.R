04c230397f028efc187f1bb92a90d8f4d8cd6d34
Joel Walker <joel@walker9.net>
2016-11-23 23:44:14 -0600

    Added blurring (untested)

--- config.R
+++ config.R
@@ -3,6 +3,8 @@ get.config <- function() {
         R.repos = "http://cran.us.r-project.org",
         arisa.fp = "./source/ARISA.CSV",
         ciliates.1.fp = "./source/CILIATES_1.CSV",
-        ciliates.2.fp = "./source/CILIATES_2.CSV"
+        ciliates.2.fp = "./source/CILIATES_2.CSV",
+        blur_stddev = 1.0,
+        blur_rows = 16
     ))
 }
--- data/repo.R
+++ data/repo.R
@@ -3,6 +3,7 @@
 # This file returns dataframes related to the CSV's that are imported into the functions
 ##############################################################################################
 library("plyr")
+library("zoo")
 
 #============================
 # get.arisa
@@ -11,25 +12,25 @@ library("plyr")
 #
 # Inputs:
 #   config...a configuration object
-# 
+#
 # Returns:
 #   a dataframe containing data from the ARISA dataset
 #   Rows correspond to a single date from which samples were taken.
-#   Columns include a formated date (date), site location (source), 
+#   Columns include a formated date (date), site location (source),
 #   and the each species of ARISA with its corresponding relative abundance
 #   determined for each corresponding date/site.
 get.arisa <- function(config) {
-    
+
     # Creates a dataframe and stores all data from the arisa file into it
     arisa <- read.csv(config[["arisa.fp"]], header = TRUE)
- 
+
     # Adds a column for classifying each testing site
     arisa$source <- substr(arisa$X, 1, 3)
     arisa$source <- gsub("TT", "T", arisa$source)
- 
+
     # Adds a column for each sample test date
     arisa$date <- as.Date(substr(arisa$X, 5, 100), "%m_%d_%y")
- 
+
     return(arisa)
 }
 
@@ -41,27 +42,27 @@ get.arisa <- function(config) {
 #
 # Inputs:
 #   config...a configuration object
-# 
+#
 # Returns:
 #   a dataframe containing data from the Ciliates (tab 1) dataset.
 #   Rows correspond to a single date from which samples were taken.
-#   Columns include a formated date (date), site location (source), 
+#   Columns include a formated date (date), site location (source),
 #   total cells per litter obtained (TotalCellsPerLiter), and columns for each Ciliate species,
-#   of which contain the cells per litter for each species taken at a specific date 
+#   of which contain the cells per litter for each species taken at a specific date
 get.ciliates.1 <- function(config) {
-    
+
     # Creates a dataframe and stores all data from the first tab of the ciliates file
     ciliates.1 <- read.csv(config[["ciliates.1.fp"]], header=TRUE)
 
     # Remove all empty/invalid rows
     ciliates.1 <- ciliates.1[!apply(is.na(ciliates.1) | ciliates.1 == "", 1, all), ]
-    
+
     # Convert date strings into R dates.
     ciliates.1$Date <- as.Date(gsub("/", "-", ciliates.1$Date), "%m-%d-%y")
-    
+
     # Rename last column, which has odd characters that R can't handle
     colnames(ciliates.1)[length(ciliates.1)] <- "TotalCellsPerLiter"
-    
+
     return(ciliates.1)
 }
 
@@ -74,34 +75,56 @@ get.ciliates.1 <- function(config) {
 #
 # Inputs:
 #   config...a configuration object
-# 
+#
 # Returns:
 #   a dataframe containing data from the Ciliates (tab 1) dataset.
 #   Rows correspond to a single date from which samples were taken.
 #   Columns include a formated date (date), site location (source),
-#   and columns for each Ciliate species, of which contain the cells 
-#   per litter for each species taken at a specific date, and the each 
-#   species of ARISA with its corresponding relative abundance determined 
+#   and columns for each Ciliate species, of which contain the cells
+#   per litter for each species taken at a specific date, and the each
+#   species of ARISA with its corresponding relative abundance determined
 #   for each corresponding date/site.
 get.all <- function(config) {
-    
+
   # Get each data set and label their species classification
   arisa <- get.arisa(config)
   ciliates.1 <- get.ciliates.1(config)
   arisa$x <- NULL
   arisa$X <- NULL
-   
-  # Stardardize each raw dataframe by removing unnecessary data columns and making 
+
+  # Stardardize each raw dataframe by removing unnecessary data columns and making
   ciliates.1 <- rename(ciliates.1, c("Date" = "date", "site" = "source"))
   ciliates.1$TotalCellsPerLiter <- NULL
   ciliates.1$month <- NULL
   ciliates.1$year <- NULL
   ciliates.1$x <- NULL
   ciliates.1$X <- NULL
-  
+
   # join using date and source
   ciliates.1$datesource <- paste(ciliates.1$date, ciliates.1$source, sep=" ")
-  merged <- merge(ciliates.1, arisa, by=c("date", "source"), all =T)
- 
-  return(merged)
+  merged <- merge(ciliates.1, arisa, by=c("date", "source"), all = T)
+
+  # Interpolate NA values
+  merged <- na.approx(merged)
+
+  # Calculate some constants
+  blur_stddev_sq <- config[['blur_stddev']] * config[['blur_stddev']]
+  gauss_factor <- 1 / sqrt(2 * pi * blur_stddev_sq)
+
+  # Run function over each row of data.
+  blurred <- t(sapply(1:nrow(merged), function(i, blur_rows) {
+    res <- merged[i,] * gauss_factor
+    for (j in 1:blur_rows) {
+      factor <- gauss_factor * exp(-j * j / (2 * blur_stddev_sq))
+      if (i - j > 0) {
+        res <- res + merged[i - j,] * factor
+      }
+      if (i + j <= nrow(merged)) {
+        res <- res + merged[i + j,] * factor
+      }
+    }
+    res
+  }, config[['blur_rows']]))
+
+  return(blurred)
 }
