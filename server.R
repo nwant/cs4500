ab67ea26333dbd0e1f0b10025fb5b9ad8e34ffe5
Nathan Want <nwant01@gmail.com>
2016-11-27 11:21:25 -0600

    ui widgets moved to top of page

--- server.R
+++ server.R
@@ -1,18 +1,29 @@
 source("./config.R")
 source("./data/repo.R")
+source("./data/filter.R")
 source("./plot/ciliates.R")
 source("./plot/arisa.R")
 # get the configuration named list and data frames from csvs
-library(shiny)
+library("corrplot")
+library("shiny")
 
-config <- get.config()
-arisa <- get.arisa(config)
-ciliates.1 <- get.ciliates.1(config)
 
 server <- function(input, output) {
+  config <- get.config()
+  df <- get.all(config)
+  species <- get.species.names(df)
 
-  # render plot for ARISA data
-  output$arisa_plot <- renderPlot({
+  output$species <- renderUI({
+    selectInput("species", "Choose Species:", as.list(c("test")))
+  })
+  
+  output$selection <- renderPlot(
+    input$specieschooser
+  )
+  
+  output$corr_matrix <- renderPlot({
+    #input$runButton
+    
     tt1 <- if(input$tt1) "TT1" else NULL
     tt2 <- if(input$tt2) "TT2" else NULL
     tt3 <- if(input$tt3) "TT3" else NULL
@@ -20,21 +31,14 @@ server <- function(input, output) {
     date.min <- input$date_min
     date.max <- input$date_max
     
-    p <- render.arisa.plot(arisa, sources, date.min, date.max)
-    print(p)
-  })
-  
-  output$ciliates_plot <- renderPlot({
-    tt1 <- if(input$tt1) "T1" else NULL
-    tt2 <- if(input$tt2) "T2" else NULL
-    tt3 <- if(input$tt3) "T3" else NULL
-    sources <- c(tt1, tt2, tt3)
-    date.min <- input$date_min
-    date.max <- input$date_max
+    hist(c(1,2,3))
+     # 
+     # f <- filter.all.data(config, df, sources, date.min, date.max)
+     # M <- cor(f)
+     # corrplot(M, method="circle")
     
-    p <- render.ciliates.plot(ciliates.1, sources, date.min, date.max)
-    print(p)
   })
+
 }
 
 
--- ui.R
+++ ui.R
@@ -1,37 +1,39 @@
+source("./controls.R")
+source("./config.R")
+source("./data/repo.R")
+source("./data/filter.R")
 library(shiny)
 
-ui <- pageWithSidebar(
-  
-  # Application title
-  headerPanel("SidePanel"),
-  
-  # Sidebar with controls to select the variable to plot against mpg
-  # and to specify whether outliers should be included
-  sidebarPanel(
-  
-    checkboxInput(inputId = "tt1",
-      label = strong("Show TT1"),
-      value = TRUE),
-      
-    checkboxInput(inputId = "tt2",
-      label = strong("Show TT2"),
-      value = TRUE),
-      
-    checkboxInput(inputId = "tt3",
-      label = strong("Show TT3"),
-      value = TRUE),
-      
-    dateInput(inputId = "date_min", "Date Start", value = "2008-01-11", min = "2008-01-11", max = "2013-08-01",
-      format = "yyyy-mm-dd", startview = "month", weekstart = 0,
-      language = "en", width = NULL),
-      
-    dateInput(inputId = "date_max", "Date End", value = "2013-08-01", min = "2008-01-11", max = "2013-08-01",
-      format = "yyyy-mm-dd", startview = "month", weekstart = 0,
-      language = "en", width = NULL)
+config <- get.config()
+df <- get.all(config)
+species <- get.species.names(df)
+df <- NULL
 
-  ),
-  mainPanel(
-    plotOutput("arisa_plot"),
-    plotOutput("ciliates_plot")
-  )
+ui <- 
+  fluidPage(
+    fluidRow(
+      column(6, 
+             chooserInput("specieschooser", "Available species", "Selected species", 
+                          species, c(), size=10, multiple=T)),
+      
+      column(3, 
+              checkboxInput(inputId = "tt1", label = strong("Show TT1"), value = TRUE),
+      
+              checkboxInput(inputId = "tt2", label = strong("Show TT2"), value = TRUE),
+    
+              checkboxInput(inputId = "tt3", label = strong("Show TT3"), value = TRUE)),
+      
+      column(3,
+              dateInput(inputId = "date_min", "Date Start", value = "2008-01-11", 
+                        min = "2008-01-11", max = "2013-08-01",format = "yyyy-mm-dd", 
+                        startview = "month", weekstart = 0, language = "en", width = NULL),
+    
+              dateInput(inputId = "date_max", "Date End", value = "2013-08-01", 
+                        min = "2008-01-11", max = "2013-08-01", format = "yyyy-mm-dd", 
+                        startview = "month", weekstart = 0, language = "en", width = NULL))
+    ),
+    
+    fluidRow(
+      plotOutput("corr_matrix")
+    )
 )
