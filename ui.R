9d42bce415ac126eafefd5b830d60ac2fad6bd5e
tjshbb <scott.t.3042@gmail.com>
2016-11-30 18:37:19 -0600

    Update ui.R

--- ui.R
+++ ui.R
@@ -5,6 +5,7 @@ ui <- bootstrapPage(
   fluidPage(
     
     sidebarPanel(
+      # Inputs for testing sites
       uiOutput("speciesSelect"), 
       checkboxInput(inputId = "tt1", label = strong("Show T1"), value = TRUE),
        
@@ -12,6 +13,7 @@ ui <- bootstrapPage(
        
       checkboxInput(inputId = "tt3", label = strong("Show T3"), value = TRUE),
   
+      # Inputs for time-slice (Date min & max)
       dateInput(inputId = "date_min", "Date Start", value = "2008-01-11", 
                  min = "2008-01-11", max = "2013-08-01",format = "yyyy-mm-dd", 
                  startview = "month", weekstart = 0, language = "en", width = NULL),
@@ -19,7 +21,8 @@ ui <- bootstrapPage(
       dateInput(inputId = "date_max", "Date End", value = "2013-08-01", 
                  min = "2008-01-11", max = "2013-08-01", format = "yyyy-mm-dd", 
                  startview = "month", weekstart = 0, language = "en", width = NULL)
-    ),mainPanel(    
+    ),mainPanel(
+      # Puts the corr matrix onto the UI
       plotOutput("corr_matrix")
     )
   )
